3133a7038977685ed110aa6cc8c9d8d8f6ecd719
Mait Lättekivi <kivimait@gmail.com>
2019-02-27 15:13:44 +0200

    panin jutumärke veits

--- src/hello.java
+++ src/hello.java
@@ -1,81 +1,76 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("We're no strangers to love
-You know the rules and so do I
-A full commitment's what I'm thinking of
-You wouldn't get this from any other guy
+        System.out.println("We're no strangers to love You know the rules and so do I " +
+                        "A full commitment 's what I' m thinking of " +
+                        "You wouldn 't get this from any other guy " +
+                        "I just wanna tell you how I 'm feeling " +
+                        "Gotta make you understand " +
+                        "Never gonna give you up " +
+                        "Never gonna let you down " +
+                        "Never gonna run around and desert you " +
+                        "Never gonna make you cry " +
+                        "Never gonna say goodbye " +
+                        "Never gonna tell a lie and hurt you " +
+                        "We 've known each other for so long " +
+                        "Your heart 's been aching, but " +
+                        "You 're too shy to say it " +
+                        "Inside, we both know what 's been going on " +
+                        "We know the game and we 're gonna play it " +
+                        "And if you ask me how I 'm feeling
+        Don 't tell me you' re too blind to see
 
-I just wanna tell you how I'm feeling
-Gotta make you understand
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
 
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
 
-We've known each other for so long
-Your heart's been aching, but
-You're too shy to say it
-Inside, we both know what's been going on
-We know the game and we're gonna play it
+        (Ooh, give you up)
+        (Ooh, give you up)
+        Never gonna give, never gonna give
+        (Give you up)
+        Never gonna give, never gonna give
+        (Give you up)
 
-And if you ask me how I'm feeling
-Don't tell me you're too blind to see
+        We 've known each other for so long
+        Your heart 's been aching, but
+        You 're too shy to say it
+        Inside, we both know what 's been going on
+        We know the game and we 're gonna play it
 
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
+        I just wanna tell you how I 'm feeling
+        Gotta make you understand
 
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
 
-(Ooh, give you up)
-(Ooh, give you up)
-Never gonna give, never gonna give
-(Give you up)
-Never gonna give, never gonna give
-(Give you up)
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
 
-We've known each other for so long
-Your heart's been aching, but
-You're too shy to say it
-Inside, we both know what's been going on
-We know the game and we're gonna play it
-
-I just wanna tell you how I'm feeling
-Gotta make you understand
-
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
-
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
-
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you
+        Never gonna give you up
+        Never gonna let you down
+        Never gonna run around and desert you
+        Never gonna make you cry
+        Never gonna say goodbye
+        Never gonna tell a lie and hurt you
 
 
-");
+        ");
     }
 }
